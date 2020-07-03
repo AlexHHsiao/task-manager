3dfd254af4297e7b28c2474e925114a592652d48
Alex Xiao <alex.hongquan.xiao@gmail.com>
2020-07-02 17:46:01 -0700

    add main router

--- src/index.js
+++ src/index.js
@@ -14,9 +14,86 @@ app.use(taskRouter);
 
 app.get('', (req, res) => {
     res.send({
-        message: 'Welcome',
-        available: {
-
+        availableRouters: {
+            user: [
+                {
+                    location: '/users/:id/avatar',
+                    method: 'GET',
+                    authentication: true
+                },
+                {
+                    location: '/users/me',
+                    method: 'GET',
+                    authentication: true
+                },
+                {
+                    location: '/users/logoutAll',
+                    method: 'POST',
+                    authentication: true
+                },
+                {
+                    location: '/users/logout',
+                    method: 'POST',
+                    authentication: true
+                },
+                {
+                    location: '/users/login',
+                    method: 'POST',
+                    authentication: true
+                },
+                {
+                    location: '/users',
+                    method: 'POST',
+                    authentication: true
+                },
+                {
+                    location: '/users/me/avatar',
+                    method: 'POST',
+                    authentication: true
+                },
+                {
+                    location: '/users/me',
+                    method: 'PATCH',
+                    authentication: true
+                },
+                {
+                    location: '/users/me/avatar',
+                    method: 'DELETE',
+                    authentication: true
+                },
+                {
+                    location: '/users/me',
+                    method: 'DELETE',
+                    authentication: true
+                }
+            ],
+            task: [
+                {
+                    location: '/tasks',
+                    method: 'GET',
+                    authentication: true
+                },
+                {
+                    location: '/tasks/:id',
+                    method: 'GET',
+                    authentication: true
+                },
+                {
+                    location: '/tasks',
+                    method: 'POST',
+                    authentication: true
+                },
+                {
+                    location: '/tasks/:id',
+                    method: 'PATCH',
+                    authentication: true
+                },
+                {
+                    location: '/tasks/:id',
+                    method: 'DELETE',
+                    authentication: true
+                }
+            ]
         }
     });
 });
